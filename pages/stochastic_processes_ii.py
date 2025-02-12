fb2073b31dce698dfd73d4a81ec427ec98f9194d
hallam-flda <141720606+hallam-flda@users.noreply.github.com>
2025-02-12 17:40:03 +0000

    added requirements

--- pages/stochastic_processes_ii.py
+++ pages/stochastic_processes_ii.py
@@ -58,10 +58,6 @@ st.header("""
           Stopping Times
 """, divider=True)
 
-st.subheader("""
-          Mathematical Properties
-""")
-
 st.markdown(r"""
 ## Mathematical Properties
 **Definition:** A stopping time $\tau$ is a random variable that represents the time at which a particular condition within a stochastic process is satisfied. The key property of a stopping time is that the decision to stop can be made based only on the information available up to that time.
@@ -95,3 +91,123 @@ As can be seen from the above, each iteration of $F_{t}$ contains all informatio
 $$ F_0 ⊆ F_1 \subseteq F_2 ... ⊆ F_t $$
 """, unsafe_allow_html=True
             )
+
+st.markdown(r"""
+## Practical Examples
+
+
+### 1.1 Bankruptcy Colours Betting
+
+The obvious example we can use this theory for is the case in which the gambler loses all of their initial bankroll.
+
+We will start by setting the customer's balance at £100 or $X_0 = 100$, what we're looking to find is where $\mathbb{E}[X_{t}] = 0$. We know that the absolute minimum time this could happen for £1 stakes would be 100 spins but winnings could greatly increase the time in which we expected to achieve a balane of zero. What we're looking for is the expected value of the stopping time,  $\mathbb{E}[\tau]$
+
+As we've seen before this can be written as a random walk like so:
+
+<br>
+
+$$
+X_{t+1} =  \begin{cases}
+X_{t} + b, & \text{with probability } p \\
+X_{t} - b, & \text{with probability } 1-p
+\end{cases}
+$$
+
+where:
+""", unsafe_allow_html=True
+            )
+
+st.write(r"""
+- $p$ is the probability of success, in this case correctly identifying a number
+- $b$ is the bet size (and also the return size in the case of betting on colours)
+- $X_t$ is the gambler's balance at time $t$
+""")
+
+st.markdown(r"""
+
+**Expected Change Per Step (Drift)**
+
+The drift $ \Delta $ is the expected change in the gambler's bankroll after one step:
+
+<br>
+
+$$ \Delta = \mathbb{E}[X_{t+1} - X_{t}] = b \cdot p - b \cdot (1 - p). = bp - b + bp $$
+
+<br>
+
+$$
+= 2bp - b = b(2p-1)
+$$
+
+<br>
+
+The formula for the expected number of steps before bankruptcy is given by
+
+<br>
+
+$$
+\mathbb{E}[\tau] = \frac{X_0}{|b\cdot(2p-1)|}
+$$
+
+<br>
+
+which for £1 stakes, a starting balance of £100 and a win probability of $\frac{18}{37}$ gives us
+
+<br>
+
+$$
+\mathbb{E}[\tau] = \frac{X_0}{b\cdot(1-2p)} = \frac{100}{1\cdot\left(1-2\cdot\left(\frac{18}{37}\right)\right)} = \frac{100}{0.027} \approx 3703
+$$
+""", unsafe_allow_html=True
+            )
+
+
+st.markdown(r"""
+### 1.2 Betting on Numbers
+
+In the previous example, the outcomes of the random walk were the previous balance $\pm 1$ because betting on colours will return your stake plus an additional unit. In the example of betting on numbers, you receive your stake plus 35 additional units, and therefore, the process can be modeled as:
+
+$$ 
+X_{t+1} =  \begin{cases}
+X_{t} + (R \cdot b), & \text{with probability } p \\
+X_{t} - b, & \text{with probability } 1-p
+\end{cases}
+$$
+
+where:
+""", unsafe_allow_html=True)
+
+st.write(
+r"""
+- $p$ is the probability of success, in this case, correctly identifying a number.
+- $b$ is the bet size.
+- $R$ is the return multiplier.
+- $X_t$ is the gambler's balance at time $t$.
+"""
+)
+
+st.markdown(r"""
+**Expected Change Per Step (Drift)**
+
+The drift $ \Delta $ is the expected change in the gambler's bankroll after one step:
+
+$$ \Delta = \mathbb{E}[X_{t+1} - X_{t}] = b \cdot p \cdot R  - b \cdot (1 - p). = bpR - b + bp $$
+
+$$
+= 2bp - b = b(2p-1)
+$$
+
+The formula for the expected number of steps before bankruptcy is given by
+
+$$
+\mathbb{E}[\tau] = \frac{X_0}{|b\cdot(R \cdot p-1)|}
+$$
+
+which for £1 stakes, a starting balance of £100, a win probability of $\frac{1}{37}$, and a payout multiplier of 36 gives us
+
+$$
+\mathbb{E}[\tau] = \frac{X_0}{|b\cdot(R \cdot p-1)|}  = \frac{100}{|1\cdot\left(36 \cdot \left(\frac{1}{37} -1\right)\right)|} = \frac{100}{0.027} \approx 3703
+$$
+
+**Hold on** that's the same result that we had for betting on colours. This is perhaps to be expected since the individual expected result of each spin is the same, and so the expected time to bankruptcy should also be the same. We know in practice customers are likely to be sensitive to big swings in balance, so the *variance* of stopping times might be more important to analyze.
+""", unsafe_allow_html=True)
