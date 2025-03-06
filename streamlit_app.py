22259273b9e180fe24d238fd0537bacbd5d3a947
hallam-flda <141720606+hallam-flda@users.noreply.github.com>
2025-03-06 14:27:39 +0000

    add ⏳ to WIP pages

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,14 +14,14 @@ pages = {
     ],
     "Maths Theory": [
         st.Page("pages/stochastic_processes.py", title="Stochastic Processes - Introduction"),
-        st.Page("pages/stochastic_processes_ii.py", title="Stochastic Processes - Stopping Times")
+        st.Page("pages/stochastic_processes_ii.py", title="Stochastic Processes - Stopping Times", icon="⏳")
     ],
     "Projects" :[
-        st.Page("pages/european_roulette_sims.py", title="European Roulette Simulations")
+        st.Page("pages/european_roulette_sims.py", title="European Roulette Simulations", icon="⏳")
     ],
     "Personal":[
-        st.Page("pages/travel_friends.py", title = "Travel Friends")#,
-        #st.Page("pages/habit_tracker_dashboard.py", title = "Habit Tracking Dashboard")
+        st.Page("pages/travel_friends.py", title = "Travel Friends"),
+        st.Page("pages/habit_tracker_dashboard.py", title = "Habit Tracking Dashboard", icon="⏳")
     ],
     "Ad-Hoc Analysis":[
         st.Page("pages/irish_pubs.py", title = "Irish Pubs of Europe")
