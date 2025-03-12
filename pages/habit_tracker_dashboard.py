0000f3458426cc0628df77e68a9464d475a5bb85
hallam-flda <141720606+hallam-flda@users.noreply.github.com>
2025-03-12 22:41:52 +0000

    added workflow for Garmin

--- pages/habit_tracker_dashboard.py
+++ pages/habit_tracker_dashboard.py
@@ -6,7 +6,7 @@ st.caption("An Embedded Looker Studio Dashboard to track my daily habits. See ex
 # Embed Looker dashboard (Replace with your Looker URL)
 looker_dashboard_url = "https://lookerstudio.google.com/embed/reporting/737ea117-2e83-45ed-8a2d-b506331e0ca1/page/6DAAF"
 
-st.components.v1.iframe(looker_dashboard_url, width=800, height=600)
+st.components.v1.iframe(looker_dashboard_url, width=800, height=1000)
 
 with st.expander("Duolingo Workflow"):
     st.header("Duolingo Daily Progress")
@@ -88,5 +88,79 @@ with st.expander("Duolingo Workflow"):
             return "Row appended successfully"
 
         
+        """
+            )
+
+with st.expander("Garmin Workflow"):
+    st.header("Garmin Daily Steps")
+    st.markdown(
+    """
+    The workflow is very similar to the Duolingo process with the exception that the code writes to a BigQuery table instead of a Google sheet. The code runs once a day at 6am to insert the previous day's data.
+    I should improve this to run for the last 7 days and replace existing values in case of failed runs, although I'm not sure how common that will be.
+
+    Code for the (unofficial) Garmin API can be found [here](https://github.com/cyberjunky/python-garminconnect/blob/master/garminconnect/__init__.py)  
+    """)
+    
+    st.subheader("Python Code")
+    st.code(
+        """
+        import os
+        import garminconnect
+        import pandas as pd
+        import numpy as np
+        from datetime import date, timedelta
+        from google.cloud import bigquery, secretmanager
+
+
+
+        # Retrieve Garmin password securely from Secret Manager
+        def get_garmin_password():
+            client = secretmanager.SecretManagerServiceClient()
+            secret_name = f"projects/{os.getenv('GCP_PROJECT_ID')}/secrets/garmin_pass/versions/latest"
+            response = client.access_secret_version(request={"name": secret_name})
+            return response.payload.data.decode("UTF-8")
+
+        # Cloud Function entry point
+        def fetch_and_store_garmin_steps(request):
+            try:
+                # Authenticate with Garmin
+                garmin_pass = get_garmin_password()
+                garmin = garminconnect.Garmin("hallamcunningham@gmail.com", garmin_pass)
+                garmin.login()
+
+                # Get yesterday's date
+                yesterday = (date.today() - timedelta(days=1)).isoformat()
+
+                # Fetch daily steps from Garmin
+                daily_steps = garmin.get_daily_steps(yesterday, yesterday)
+                if not daily_steps:
+                    return "No data retrieved from Garmin", 400
+
+            # Convert to Pandas DataFrame
+                daily_steps_df = pd.DataFrame(daily_steps)
+                print(daily_steps_df)
+
+                # Ensure all values in the DataFrame are converted to Python native types
+                daily_steps_dict = daily_steps_df.iloc[0].to_dict()
+                daily_steps_dict = {key: value.item() if hasattr(value, "item") else value for key, value in daily_steps_dict.items()}
+
+                if "totalDistance" in daily_steps_dict:
+                    daily_steps_dict["totalDistanceMeters"] = daily_steps_dict.pop("totalDistance")
+                    daily_steps_dict["dailyStepGoal"] = daily_steps_dict.pop("stepGoal")
+
+                # Insert into BigQuery
+                client = bigquery.Client()
+                table_id = "tonal-run-447413-c0.garmin.daily_steps"
+
+                errors = client.insert_rows_json(table_id, [daily_steps_dict])
+
+                if errors:
+                    return f"Error inserting data: {errors}", 500
+
+                return f"Data for {yesterday} successfully inserted into BigQuery!", 200
+
+            except Exception as e:
+                return f"Error: {str(e)}", 500
+
         """
             )
