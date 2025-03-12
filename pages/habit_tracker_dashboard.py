89262faf75bd31c8e4123f2976ace3d807a89037
hallam-flda <141720606+hallam-flda@users.noreply.github.com>
2025-03-12 17:13:33 +0000

    habit tracker has workflow write up and looker dashboard embedded

--- pages/habit_tracker_dashboard.py
+++ pages/habit_tracker_dashboard.py
@@ -1,24 +1,92 @@
 import streamlit as st
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-import plotly.express as px
 
-sheet_id = '1q1xDAhCBlIs9VBPu698HSCysR72GWLJRu_0EBogYTgk'
-csv_url = f'https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv'
+st.title("Habit Tracking Dashboard")
+st.caption("An Embedded Looker Studio Dashboard to track my daily habits. See expanders at bottom for workflows.")
 
-df = pd.read_csv(csv_url)
+# Embed Looker dashboard (Replace with your Looker URL)
+looker_dashboard_url = "https://lookerstudio.google.com/embed/reporting/737ea117-2e83-45ed-8a2d-b506331e0ca1/page/6DAAF"
 
-# Lag the values from the previous entry in database
-df['spanish_lagged'] = df.spanish.shift(1)
-df['portuguese_lagged'] = df.portuguese.shift(1)
+st.components.v1.iframe(looker_dashboard_url, width=800, height=600)
 
-# Calculate difference with previous values
-df['spanish_xp_change'] = df.spanish - df.spanish_lagged
-df['portuguese_xp_change'] = df.portuguese - df.portuguese_lagged
-df['date'] = pd.to_datetime(df['date']).dt.date
+with st.expander("Duolingo Workflow"):
+    st.header("Duolingo Daily Progress")
+    st.markdown(
+    """
+    A python script hosted and scheduled on GCP runs every 4 hours to extract my
+    XP values from the duolingo API. This then writes to a Google Sheet which is used as a 
+    data source in Looker Studio. 
+    
+    Because of changes made to the duolingo API you can no longer
+    access this information with login details, instead you have to provide a session cookie as if you
+    were logged in through your browser. Details: [API](https://github.com/KartikTalwar/duolingo?tab=readme-ov-file) and fix for
+    login details not working [here](https://github.com/KartikTalwar/Duolingo/issues/128).
+
+    This could be improved by writing directly to a database in GCP but will suffice for now. I decided against coding directly in
+    streamlit to avoid revealing my API details (not that they're particularly sensitive). I've since found out you can
+    use secrets in streamlit so this was not an issue anyway.
+    """)
+    
+    st.subheader("Python Code")
+    st.code(
+        """
+        import duolingo
+        import datetime
+        import pandas as pd
+        import json
+        import inspect
+        import numpy as np
+        import gspread
+        from google.cloud import secretmanager
 
 
-fig = px.bar(df, x="date", y=["spanish_xp_change","portuguese_xp_change"], title="Daily Duolingo XP Change by Language")
+        def google_sheets_function(request):
 
-st.plotly_chart(fig)
+            gcp_creds_json = {
+        "type": "service_account",
+        "project_id": "tonal-run-447413-c0",
+        "private_key_id": PRIVATE_KEY_ID,
+        "private_key": PRIVATE_KEY,
+        "client_email": CLIENT_EMAIL,
+        "client_id": CLIENT_ID,
+        "auth_uri": AUTH_URI,
+        "token_uri": TOKEN_URI,
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": CLIENT_X509_CERT_URL,
+        "universe_domain": "googleapis.com"
+        }
+            print(gcp_creds_json)
+
+            source = inspect.getsource(duolingo)
+            new_source = source.replace('jwt=None', 'jwt')
+            new_source = source.replace('self.jwt = None', ' ')
+            exec(new_source, duolingo.__dict__)
+            
+            lingo = duolingo.Duolingo(USER, jwt=JWT)
+            api_lang_info = lingo.user_data.languages
+
+            # Filter for Spanish and Portuguese points
+            xp_data = {lang['language_string'].lower(): lang['points']
+                    for lang in api_lang_info if lang['language_string'] in ['Spanish', 'Portuguese']}
+
+            db_dict = {
+                "datetime": datetime.datetime.now().isoformat(),
+                "xp": xp_data
+            }
+
+            db_df2 = pd.DataFrame([db_dict])
+            db_df_final = pd.concat([db_df2.drop(['xp'], axis=1), db_df2['xp'].apply(pd.Series)], axis=1)
+            row_to_append = db_df_final.iloc[0].astype(object).to_list()
+            row_to_append = [int(x) if isinstance(x, np.int64) else x for x in row_to_append]
+
+            # Initialise Google Sheets client
+            gc = gspread.service_account_from_dict(gcp_creds_json)
+            sheet_url = '1q1xDAhCBlIs9VBPu698HSCysR72GWLJRu_0EBogYTgk'  # Google Sheet key
+            sh = gc.open_by_key(sheet_url)
+            worksheet = sh.sheet1
+            worksheet.append_row(row_to_append)
+
+            return "Row appended successfully"
+
+        
+        """
+            )
