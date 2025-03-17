f100ea8e7f797cbb89db15017ab6c8c5dbe0bfc2
hallam-flda <141720606+hallam-flda@users.noreply.github.com>
2025-03-17 20:36:01 +0000

    reuploaded with activities showing

--- pages/garmin_activity_map.py
+++ pages/garmin_activity_map.py
@@ -4,7 +4,7 @@ import streamlit.components.v1 as components
 st.title("Garmin Activity Map")
 st.caption("An interactive plot of all my recorded Garmin activities from 01/01/2023 - 15/02/2025, arcs represent journeys between actvities with a distance >30km from one another.")
 
-kepler_map_path = "static/travel_log.html"
+kepler_map_path = "static/travel_and_activities.html"
 
 with open(kepler_map_path, "r", encoding="utf-8") as f:
     html_string = f.read()
