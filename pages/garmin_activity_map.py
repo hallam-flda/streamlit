c66834c26a78eb0ba9e858ac9c45358c7c2b9ccf
hallam-flda <141720606+hallam-flda@users.noreply.github.com>
2025-03-20 17:32:46 +0000

    changed map to be hosted on github as loading times too long

--- pages/garmin_activity_map.py
+++ pages/garmin_activity_map.py
@@ -1,13 +1,8 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
+
 st.title("Garmin Activity Map")
-st.caption("An interactive plot of all my recorded Garmin activities from 01/01/2023 - 15/02/2025, arcs represent journeys between actvities with a distance >30km from one another.")
+st.caption("Map can be found [here!](https://hallam-flda.github.io/garmin_map/)")
 
-kepler_map_path = "static/travel_and_activities.html"
 
-with open(kepler_map_path, "r", encoding="utf-8") as f:
-    html_string = f.read()
-
-# Embed in Streamlit
-components.html(html_string, height=600, width=1000, scrolling=True)
