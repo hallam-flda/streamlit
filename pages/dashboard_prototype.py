62d14dac76672b91a1bca81d3d628e0d3d5fa786
hallam-flda <141720606+hallam-flda@users.noreply.github.com>
2025-04-12 17:59:20 +0000

    key concept works, dashboard to be migrated to other repo

--- pages/dashboard_prototype.py
+++ pages/dashboard_prototype.py
@@ -1,40 +1,68 @@
-
 import streamlit as st
 import pandas as pd
 from utils import fbref
 
-home_squad_df = pd.read_csv("data/data/fbref_dashboard/palace_squad_24_25.csv")
-home_squad_df['team'] = 'Crystal Palace'
- 
-# prem_table = pd.read_csv("data/data/fbref_dashboard/prem_table.csv")
-# st.dataframe(prem_table.iloc[:,1:-4])
+st.caption("This app will look better on a standalone site where I can better manage layout and appearance at a global level ⏳⏳⏳")
 
 prem_table_ha = pd.read_csv("data/data/fbref_dashboard/prem_table_ha.csv")
 team_list = set(prem_table_ha.Squad)
 
-home_team = st.selectbox(
-    "Home Team",
-    team_list,
-    index=None,
-    placeholder="Select Home Team...",
-)
+league_stats, prob_output = st.columns([1,1])
 
-if home_team:
-    home_stats = prem_table_ha[prem_table_ha["Squad"] == home_team]
-    st.dataframe(home_stats.iloc[:,1:])
+with league_stats: 
+    st.header("League Form", divider = True)
+    home_team = st.selectbox(
+        "Home Team",
+        team_list,
+        index=None,
+        placeholder="Select Home Team...",
+    )
 
-away_team = st.selectbox(
-    "Away Team",
-    team_list,
-    index=None,
-    placeholder="Select Away Team...",
-)
+    if home_team:
+        home_stats = prem_table_ha[prem_table_ha["Squad"] == home_team]
+        st.dataframe(home_stats.iloc[:,1:])
 
-if away_team:
-    away_stats = prem_table_ha[prem_table_ha["Squad"] == away_team]
-    st.dataframe(away_stats.iloc[:,1:])
+    away_team = st.selectbox(
+        "Away Team",
+        team_list,
+        index=None,
+        placeholder="Select Away Team...",
+    )
 
-home_col, away_col, output_probs = st.columns([1,1,2])
+    if away_team:
+        away_stats = prem_table_ha[prem_table_ha["Squad"] == away_team]
+        st.dataframe(away_stats.iloc[:,1:])
 
-with home_col:
+with prob_output:
+    st.header("Probability Output", divider = True)
+    if home_team:
+        home_summary_line = f'{home_team} have played {home_stats.iloc[0].Home_MP} matches at home with a cumulative xG of {home_stats.iloc[0].Home_xG} averaging at {round(home_stats.iloc[0].Home_xG/home_stats.iloc[0].Home_MP,2)} xG per game '
+        st.write(home_summary_line)
+
+    if away_team:  
+        away_summary_line = f'{away_team} have played {away_stats.iloc[0].Away_MP} matches away with a cumulative xG of {away_stats.iloc[0].Away_xG} averaging at {round(away_stats.iloc[0].Away_xG/away_stats.iloc[0].Away_MP,2)} xG per game '
+        st.write(away_summary_line)
+
+
+
+squad_stats_df = pd.read_csv("data/data/fbref_dashboard/all_prem_squads.csv")
+
+
+home_squad_df = squad_stats_df[squad_stats_df["Team"] == home_team]
+away_squad_df = squad_stats_df[squad_stats_df["Team"] == away_team]
+
+home_team_squad_stats, away_team_squad_stats = st.columns([1,1])
+
+with home_team_squad_stats:
+    st.header("Home Team Player Stats", divider = True)
     st.dataframe(home_squad_df)
+
+
+with away_team_squad_stats:
+    st.header("Away Team Player Stats", divider = True)
+    st.dataframe(away_squad_df)
+
+# home_col, away_col, output_probs = st.columns([1,1,2])
+
+# with home_col:
+#     st.dataframe(home_squad_df)
