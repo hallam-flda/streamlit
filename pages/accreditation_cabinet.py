749afc58e5743e2de8b8ddf9d6d35b5bd8caa852
hallam-flda <141720606+hallam-flda@users.noreply.github.com>
2025-04-15 22:26:08 +0000

    fixed broken credly link - not really a permanent solution

--- pages/accreditation_cabinet.py
+++ pages/accreditation_cabinet.py
@@ -7,33 +7,10 @@ st.caption("A space to keep all the nice badges you get for completing online tr
 
 
 st.subheader("Clickhouse")
-col1, col2, col3 = st.columns([1,1,1])
+st.caption("Credly doesn't allow for embedding :( link [here](https://www.credly.com/users/hallam-cunningham.9e677446) if you want to validate")
+st.image("media/credly.png", width = 800)
 
-with col1:
-    components.iframe(
-        "https://www.credly.com/badges/55b867b1-b3a3-4a76-9563-f00c279d5f5a/public_url",
-        width=200,
-        height=300,
-        scrolling=True
-    )
 
-with col2:
-    components.iframe(
-        "https://www.credly.com/badges/6c173c4f-f963-416b-be64-60bebe3f7199/public_url",
-        width=200,
-        height=300,
-        scrolling=True
-    )
-
-with col3:
-    components.iframe(
-        "https://www.credly.com/badges/f2fe05a6-f2a8-4296-b225-985d6b55bfaa/public_url",
-        width=200,
-        height=300,
-        scrolling=True
-    )
-
-    
 st.subheader("Dbt")
 
 col4, col5, col6 = st.columns([1,1,1])
@@ -41,7 +18,7 @@ col4, col5, col6 = st.columns([1,1,1])
 with col4:
         components.iframe(
         "https://credentials.getdbt.com/embed/dff728a0-039e-44c6-845a-c3c538ac99b6",
-        width=200,
-        height=300,
+        width=400,
+        height=400,
         scrolling=True
     )
