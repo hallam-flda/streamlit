8517c9fba0dba7e282e0027d8f352285ef74c08a
hallam-flda <141720606+hallam-flda@users.noreply.github.com>
2025-04-15 22:32:15 +0000

    used images instead, looks a lot better :)

--- pages/accreditation_cabinet.py
+++ pages/accreditation_cabinet.py
@@ -7,10 +7,21 @@ st.caption("A space to keep all the nice badges you get for completing online tr
 
 
 st.subheader("Clickhouse")
-st.caption("Credly doesn't allow for embedding :( link [here](https://www.credly.com/users/hallam-cunningham.9e677446) if you want to validate")
-st.image("media/credly.png", width = 800)
+st.caption("Credly doesn't allow direct embedding of my profile but you can validate these [here](https://www.credly.com/users/hallam-cunningham.9e677446)")
 
 
+col1, col2, col3 = st.columns([1,1,1])
+
+with col1:
+    st.image("media/accreds/getting-started-with-clickhouse.png")
+
+with col2:
+    st.image("media/accreds/clickhouse-architecture.png")
+    
+with col3:
+    st.image("media/accreds/modeling-data-for-clickhouse.png")
+
+    
 st.subheader("Dbt")
 
 col4, col5, col6 = st.columns([1,1,1])
@@ -19,6 +30,6 @@ with col4:
         components.iframe(
         "https://credentials.getdbt.com/embed/dff728a0-039e-44c6-845a-c3c538ac99b6",
         width=400,
-        height=400,
+        height=600,
         scrolling=True
     )
